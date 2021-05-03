2d9c026b0ef654d3c8a8ae0380be83338e4eb5d9
Jaskier <zooom.83@mail.ru>
2021-05-03 17:11:36 +0300

    fix test main page

--- test_main_page.py
+++ test_main_page.py
@@ -8,6 +8,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
+    login_page = LoginPage(browser, page.go_to_login_page())
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
@@ -15,24 +17,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
-
-def test_guest_should_see_login_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_form()
-
-
-def test_guest_should_see_register_form(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_register_form()
-
-
-def test_login_url(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
-    page = LoginPage(browser, link)
-    page.open()
-    page.should_be_login_url()
